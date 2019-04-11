9f06fc5b7be2643df343f1021d36c807197c4e75
SergiVG <sergivillalobosgascon@enti.cat>
2019-04-11 16:48:45 +0200

    Mobile walls implemented. Steam VR fixed. No tested.

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public static class Constants
 {
-    public static string IP = "127.0.0.1";
-    public static int PORT = 33333;
+    public static string IP = "10.40.0.116";
+    public static int PORT = 8888;
 
     public static int BUFF_SIZE = 8192;
 
