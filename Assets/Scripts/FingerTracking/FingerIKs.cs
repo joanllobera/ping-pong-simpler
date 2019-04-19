64775cf8f62c0e688f0f662a1a21e3edd23af31e
Daniel Bruna <danielbruna1997@gmail.com>
2019-04-19 12:15:19 +0200

    NEW finger IKs script and prefab

---
+++ Assets/Scripts/FingerTracking/FingerIKs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerIKs : MonoBehaviour {
+
+    [SerializeField] private Transform[] _handMalcomPoints;
+    [SerializeField] private HandRenderer _leftHand;
+    private Vector3[] _handMalcomPointsRegister = new Vector3[21];
+    private Quaternion _handMalcomBaseRotationRegister;
+
+	// Use this for initialization
+	void Awake () {
+        _handMalcomPointsRegister = new Vector3[_handMalcomPoints.Length];
+        for (int i = 0; i < _handMalcomPoints.Length; ++i) {
+            _handMalcomPointsRegister[i] = _handMalcomPoints[i].position;
+        }
+        _handMalcomBaseRotationRegister = _handMalcomPoints[0].transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (_leftHand.isLeft) {
+            if (_leftHand.Points[0].activeSelf) {
+                _handMalcomPoints[0].position = _leftHand.Points[0].transform.position;
+                _handMalcomPoints[0].rotation = _leftHand.Points[0].transform.rotation;
+            } else {
+                _handMalcomPoints[0].position = _handMalcomPointsRegister[0];
+                _handMalcomPoints[0].rotation = _handMalcomBaseRotationRegister;
+            }
+            for (int i = 1; i < _leftHand.Points.Count; ++i) {
+                if (_leftHand.Points[i].activeSelf) {
+                    _handMalcomPoints[i].position = _leftHand.Points[i].transform.position;
+                } else { _handMalcomPoints[i].position = _handMalcomPointsRegister[i]; }
+            }
+        }
+	}
+}
