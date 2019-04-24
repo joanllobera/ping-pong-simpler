3c788618c1e5d3efdd3a8e5d7acb92c41e11207b
Edwarkun <edwarkun@gmail.com>
2019-04-24 17:55:38 +0200

    Finger tracking working in scene. Point matching to hand not working (only wrist with rot Y: 90 | Z: -90)

--- Assets/Scripts/FingerTracking/FingerIKs.cs
+++ Assets/Scripts/FingerTracking/FingerIKs.cs
@@ -13,26 +13,26 @@ public class FingerIKs : MonoBehaviour {
 	void Awake () {
         _handMalcomPointsRegister = new Vector3[_handMalcomPoints.Length];
         for (int i = 0; i < _handMalcomPoints.Length; ++i) {
-            _handMalcomPointsRegister[i] = _handMalcomPoints[i].position;
+            _handMalcomPointsRegister[i] = _handMalcomPoints[i] != null ? _handMalcomPoints[i].position : Vector3.zero;
         }
         _handMalcomBaseRotationRegister = _handMalcomPoints[0].transform.rotation;
     }
 
     // Update is called once per frame
-    void Update () {
+    void LateUpdate () {
         if (_leftHand.isLeft) {
             if (_leftHand.Points[0].activeSelf) {
                 _handMalcomPoints[0].position = _leftHand.Points[0].transform.position;
-                _handMalcomPoints[0].rotation = _leftHand.Points[0].transform.rotation;
-            } else {
+                _handMalcomPoints[0].rotation = _leftHand.Points[0].transform.rotation; //(Y:90, Z: -90)
+            }/* else {
                 _handMalcomPoints[0].position = _handMalcomPointsRegister[0];
                 _handMalcomPoints[0].rotation = _handMalcomBaseRotationRegister;
-            }
-            for (int i = 1; i < _leftHand.Points.Count; ++i) {
-                if (_leftHand.Points[i].activeSelf) {
+            }*/
+            /*for (int i = 1; i < _leftHand.Points.Count; ++i) {
+                if (_leftHand.Points[i].activeSelf && _handMalcomPoints[i] != null) {
                     _handMalcomPoints[i].position = _leftHand.Points[i].transform.position;
-                } else { _handMalcomPoints[i].position = _handMalcomPointsRegister[i]; }
-            }
+                } else if(_handMalcomPoints[i] != null) { _handMalcomPoints[i].position = _handMalcomPointsRegister[i]; }
+            }*/
         }
 	}
 }
