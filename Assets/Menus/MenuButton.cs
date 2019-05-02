c8081dea0e43b8df096860267523390e86ce449b
Edwarkun <edwarkun@gmail.com>
2019-05-02 20:21:04 +0200

    Menu changes and input testing. The input doesnt work.

--- Assets/Menus/MenuButton.cs
+++ Assets/Menus/MenuButton.cs
@@ -6,29 +6,72 @@ using UnityEngine.SceneManagement;
 public class MenuButton : MonoBehaviour {
 
 
+    
+    public MenuManager mm;
+    private MeshRenderer meshRenderer;
+
+    public enum ButtonID { PVP, PVAI, CONTROLS, SETTINGS, EXIT}
+    public ButtonID buttonID;
     public string sceneName;
 
-	// Use this for initialization
-	void Start () {
-		
+    //fade effect
+    bool isFading = false;
+    float fadingValue = 0f;
+
+    // Use this for initialization
+    void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (isFading)
+        {
+            FadeOut(ref fadingValue);
+        }
 	}
 
     void OnTriggerEnter(Collider col)
     {
-        if (sceneName == "")
+        meshRenderer.material = mm.orangeBall;
+        //if (sceneName == "")
+        //{
+        //    Debug.Log("sceneName is empty string");
+        //    Application.Quit();
+        //}
+        //else
+        //{
+        //    Debug.Log("opening newScene");
+        //    SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        //}
+
+        switch (buttonID)
         {
-            Debug.Log("sceneName is empty string");
-            Application.Quit();
+            case ButtonID.PVP:
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+                break;
+            case ButtonID.PVAI:
+                Debug.Log("Load Ai Scene");
+                break;
+            case ButtonID.CONTROLS:
+                break;
+            case ButtonID.SETTINGS:
+                break;
+            case ButtonID.EXIT:
+                Application.Quit();
+                break;
         }
-        else
-        {
-            Debug.Log("opening newScene");
-            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-        }
+        
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        meshRenderer.material = mm.whiteBall;
+    }
+
+    public void FadeOut(ref float fader)
+    {
+
     }
 }
