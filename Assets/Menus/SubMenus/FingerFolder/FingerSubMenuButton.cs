59b112be6304e0930bb69f8d05ef52ccced30c86
AdriaOrtizN <adriaortiznavarro@enti.cat>
2019-05-07 13:08:34 +0200

    Placeholder subemenu created

---
+++ Assets/Menus/SubMenus/FingerFolder/FingerSubMenuButton.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FingerSubMenuButton : MonoBehaviour {
+
+    public MenuManager mm;
+    private MeshRenderer meshRenderer;
+
+    public enum ButtonID { ACTIVATE, RETURN }
+    public ButtonID buttonID;
+    public string sceneName;
+
+    //fade effect
+    bool isFading = false;
+    float fadingValue = 0f;
+
+    private bool isActive;
+    // Use this for initialization
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        isActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isFading)
+        {
+            FadeOut(ref fadingValue);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        meshRenderer.material = mm.orangeBall;
+        //if (sceneName == "")
+        //{
+        //    Debug.Log("sceneName is empty string");
+        //    Application.Quit();
+        //}
+        //else
+        //{
+        //    Debug.Log("opening newScene");
+        //    SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        //}
+
+        switch (buttonID)
+        {
+            case ButtonID.ACTIVATE:
+                if (isActive)
+                {
+                    isActive = false;
+                }else if (!isActive)
+                {
+                    isActive = true;
+                }
+                else
+                {
+                    Debug.Log("Unexpected Error");
+                }
+                break;
+
+            case ButtonID.RETURN:
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+                break;
+            
+        }
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        meshRenderer.material = mm.whiteBall;
+    }
+
+    public void FadeOut(ref float fader)
+    {
+
+    }
+}
