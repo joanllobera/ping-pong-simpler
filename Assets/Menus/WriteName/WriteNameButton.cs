541609e465eacc1168196ced1426a22fea443af7
Ferran Illa <ferran.illa26@gmail.com>
2019-05-09 12:02:30 +0200

    Write Name Scene made.

---
+++ Assets/Menus/WriteName/WriteNameButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WriteNameButton : MonoBehaviour {
+
+    WriteNameManager manager;
+    public  WriteNameManager.OnButton buttonType;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.Find("NameManager").GetComponent<WriteNameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        manager.onButton = buttonType;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        manager.onButton = WriteNameManager.OnButton.Nothing;
+    }
+}
